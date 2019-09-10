1420874925d881f7dfc80c74d41570a9a1198af9
Leghort <leghort@outlook.fr>
2019-09-10 22:01:40 +0200

    init depo

---
+++ modinfo.lua
@@ -0,0 +1,20 @@
+-- This information tells other players more about the mod
+name = "French Language [DST]"
+version = "2.8"
+description = "Une traduction française du jeu Don't Starve Together\nainsi que certain mod.Les mods traduits en plus du jeu\nsont listé si dessous.\n\nMods traduit:\n- Wolf [DST]\n- Chilly willy\n- Wilbur, The Pirate\n- San(Princess Mononoke)\n\nPS : Ce mod a pour base la traduction de EALILY & Orka\nun grand merci a eux.\nMedaey\n\nVersion: "..version
+author = "Medaey"
+
+forumthread = ""
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Can specify a custom icon for this mod!
+--icon_atlas = "modicon.xml"
+--icon = "modicon.tex"
+
+server_filter_tags = {"fr", "FR", "French", "Francais", "France", "Medaey", "team-68.fr"}
+
+dont_starve_compatible = true
+dst_compatible = true
+all_clients_require_mod=false
+client_only_mod=true
