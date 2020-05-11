a42d031e5a0f72bbe573f32dea9ed70fff5172a2
Leghort <leghort@outlook.fr>
2020-05-11 18:12:12 +0200

    3.1.23.1.23.1.2

--- workshop-1462877774/modinfo.lua
+++ workshop-1462877774/modinfo.lua
@@ -1,18 +1,18 @@
 -- This information tells other players more about the mod
 name = "Patch Francais [DST]"
-version = "3.1.1"
-description = "Une traduction française du jeu Don't Starve Together\nainsi que certain mod.Les mods traduits en plus du jeu\nsont listé si dessous.\n\nMods traduit:\n-[DST] Crash Bandicoot\n-Chilly willy\n-San(Princess Mononoke)\n-Wilbur, The Pirate\n-Wolf [DST]\n\nPS : Ce mod a pour base la traduction de EALILY & Orka\nun grand merci a eux.\nMedaey\n\nVersion: "..version
-author = "Medaey"
+version = "3.1.2"
+description = "Une traduction française du jeu Don't Starve Together\nainsi que certain mod.Les mods traduits en plus du jeu\nsont listé si dessous.\n\nMods traduit:\n-[DST] Crash Bandicoot\n-Chilly willy\n-San(Princess Mononoke)\n-Wilbur, The Pirate\n-Wolf [DST]\n\nPS : Ce mod a pour base la traduction de EALILY & Orka\nun grand merci a eux.\n\nVersion: "..version
+author = "ArchMage,Rafi,Yokta,Medaey"
 
 forumthread = ""
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 10
 
 -- Can specify a custom icon for this mod!
---icon_atlas = "modicon.xml"
---icon = "modicon.tex"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 
-server_filter_tags = {"FR", "fr", "French", "Francais" , "Medaey", "team-68.fr"}
+server_filter_tags = {"FR", "fr", "French", "Francais"}
 
 dont_starve_compatible = true
 dst_compatible = true
