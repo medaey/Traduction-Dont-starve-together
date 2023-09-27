76e7601b69ec0f5863f663e7cfa216db2356a30f
medaey <medaey@hotmail.com>
2023-09-27 06:02:45 +0200

    23.09.27.0546

--- workshop-1462877774/modinfo.lua
+++ workshop-1462877774/modinfo.lua
@@ -1,8 +1,8 @@
 -- This information tells other players more about the mod
 name = "Patch Francais [DST]"
-version = "23.09.24.0244"
+version = "23.09.27.0546"
 description = "Traduction française du jeu Don't Starve Together\nainsi que certain mods.\n\nPS : Ce mod a pour base la traduction de EALILY & Orka\nun grand merci a eux.\n\nVersion: "..version
-author = " Medaey, ArchMage, Maze, Lenmir, Rafi, Yokta, Angelfire92"
+author = " Medaey, ArchMage, Khiwi, Maze, Lenmir, Rafi, Yokta, Angelfire92, CurlySheep"
 
 forumthread = ""
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
